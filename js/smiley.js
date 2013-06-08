4e41dbbb412d3170ecb5b2e5c322ece7c467c0a4
jhuang314 <jh3141@gmail.com>
2013-06-07 22:39:03 -0400

    finished face

--- js/smiley.js
+++ js/smiley.js
@@ -11,6 +11,19 @@ $(function() {
 	ctx.arc(150, 50, 25, 0, Math.PI * 2, true);
 	ctx.fill();
 
-	
+	ctx.fillStyle = "red";
+	ctx.beginPath();
+	ctx.moveTo(100, 75);
+	ctx.lineTo(75, 125);
+	ctx.lineTo(125,125);
+	ctx.fill();
+
+	ctx.strokeStyle = "green";
+	ctx.beginPath();
+	ctx.scale(1, 0.5);
+	ctx.arc(100, 300, 75, Math.PI, 0, true);
+	ctx.closePath();
+	ctx.stroke();
+
 
 	});
